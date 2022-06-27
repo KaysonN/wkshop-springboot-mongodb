c246190718a10500e2b2b836edae27635f26352b
KaysonN <carloskayson@hotmail.com>
2022-06-27 15:01:55 -0300

    Primeiro commit: estrutura base do projeto

---
+++ src/main/java/com/kayson/wshopmongo/WshopMongoApplication.java
@@ -0,0 +1,13 @@
+package com.kayson.wshopmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WshopMongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WshopMongoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kayson/wshopmongo/WshopMongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kayson.wshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WshopMongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
