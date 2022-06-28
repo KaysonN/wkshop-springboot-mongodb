22a223044fe5a0f91c7c417caf3ba14369c3f15b
KaysonN <carloskayson@hotmail.com>
2022-06-28 15:43:23 -0300

    Usando padrão DTO para retornar usuários de um banco de dados

--- src/main/java/com/kayson/wshopmongo/resources/UserResource.java
+++ src/main/java/com/kayson/wshopmongo/resources/UserResource.java
@@ -1,6 +1,7 @@
 package com.kayson.wshopmongo.resources;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.kayson.wshopmongo.domain.User;
+import com.kayson.wshopmongo.dto.UserDTO;
 import com.kayson.wshopmongo.services.UserService;
 
 //@Informa que a classe é um recurso Rest
@@ -22,11 +24,11 @@ public class UserResource {
 
 	// retorna uma lista de usuarios=>busca todos
 	@RequestMapping(method = RequestMethod.GET) // @GetMapping
-	public ResponseEntity<List<User>> findAll() {
+	public ResponseEntity<List<UserDTO>> findAll() {
 
 		// lista de User's
 		List<User> list = service.findAll();
-
-		return ResponseEntity.ok().body(list);
+		List<UserDTO> listDTO = list.stream().map(x->new UserDTO(x)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDTO);
 	}
 }
