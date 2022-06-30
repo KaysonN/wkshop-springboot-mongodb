f90f6a2d23720da0382d131338a8321b47d39c5e
KaysonN <carloskayson@hotmail.com>
2022-06-30 15:37:59 -0300

    Consulta simples com @Query e query methods

--- src/main/java/com/kayson/wshopmongo/repository/PostRepository.java
+++ src/main/java/com/kayson/wshopmongo/repository/PostRepository.java
@@ -1,6 +1,9 @@
 package com.kayson.wshopmongo.repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.kayson.wshopmongo.domain.Post;
@@ -9,4 +12,8 @@ import com.kayson.wshopmongo.domain.Post;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
 
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+	List<Post> searchTitle(String txt);
+	
+	List<Post> findByTitleContainingIgnoreCase(String text);
 }
--- src/main/java/com/kayson/wshopmongo/resources/PostResource.java
+++ src/main/java/com/kayson/wshopmongo/resources/PostResource.java
@@ -1,27 +1,39 @@
 package com.kayson.wshopmongo.resources;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.kayson.wshopmongo.domain.Post;
+import com.kayson.wshopmongo.resources.util.URL;
 import com.kayson.wshopmongo.services.PostService;
 
 //@Informa que a classe é um recurso Rest
 //@Caminho do endpoint(".......s")
 @RestController
-@RequestMapping(value="/posts")
+@RequestMapping(value = "/posts")
 public class PostResource {
 
 	@Autowired
 	private PostService service;
 
-	@RequestMapping(value="/{id}", method=RequestMethod.GET)
- 	public ResponseEntity<Post> findById(@PathVariable String id) {
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public ResponseEntity<Post> findById(@PathVariable String id) {
 		Post obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);
+	}
+
+	@RequestMapping(value = "/titlesearch", method = RequestMethod.GET)
+	public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "") String text) {
+		// decodifica o text
+		text = URL.decodeParam(text);
+		List<Post> list = service.findByTitle(text);
+		return ResponseEntity.ok().body(list);
 	}
 }
